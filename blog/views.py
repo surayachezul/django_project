6b15451f6cfe286688149d1decf3bf7ae0685b09
Suraya <nursuraya99@gmail.com>
2021-03-23 22:17:00 +0800

    update master layout:base.html

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'Suraya',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'March 18, 2021'
-    },
-    {
-        'author' : 'Ali',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'March 19, 2021'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
